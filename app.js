fecd8b0401b3206888a5e50879567c3561db4684
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-15 22:04:17 -0500

    adding bind

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Becoming a vue ninja'
+        title: 'Becoming a vue ninja',
+        name: 'emmanuel',
+        url: 'http://www.youtube.com',
+        classes: ['one', 'two']
     },
     methods:{
-        greet(){
-            
+        greet(time){
+            return `Hello and good ${time}, ${this.name} `
         }
     }
 })
