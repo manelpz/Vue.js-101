a29e8ab049cb058730c0c825398e57713be56fb6
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-16 21:38:26 -0500

    v-on:click

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
         title: 'Becoming a vue ninja',
+        wage: 10,
         name: 'emmanuel',
         url: 'http://www.youtube.com',
         classes: ['one', 'two']
@@ -9,6 +10,9 @@ new Vue({
     methods:{
         greet(time){
             return `Hello and good ${time}, ${this.name} `
+        },
+        changeWage(amount){
+            this.wage += amount
         }
     }
 })
