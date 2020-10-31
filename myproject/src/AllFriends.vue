e1dbf8bc8dba9edc626e2dc083201b02bec3d181
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-30 23:37:41 -0600

    adding template AllFriends

---
+++ myproject/src/AllFriends.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="all-friends">
+        <h2>all friends</h2>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:'AllFriends',
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+
+
+<style>
+    
+</style>
