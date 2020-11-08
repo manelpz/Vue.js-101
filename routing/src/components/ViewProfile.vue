650b177f353056e119f7bb2778a437cdd59dc7ef
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-08 12:04:17 -0600

    adding profile

--- routing/src/components/ViewProfile.vue
+++ routing/src/components/ViewProfile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="view-profile">
-        <h2>Profile</h2>
+        <h2>Profile for {{userId}}</h2>
     </div>
 </template>
 
@@ -9,12 +9,12 @@
         name:'ViewProfile',
         data () {
             return{
-
+                userId: this.$route.params.user_id
             }
         }
     }
 </script>
 
 <style>
-    
+
 </style>
