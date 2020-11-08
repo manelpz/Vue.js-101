7b28f551f90f4bcc59a42990c3deb841459099b9
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-07 21:32:26 -0600

    adding view profile

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="main-nav">
         <ul>
             <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/About">About</router-link></li>
+            <li><router-link :to="{name:'About'}">About</router-link></li>
         </ul>
     </nav>
 </template>
---
+++ routing/src/components/ViewProfile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="view-profile">
+        <h2>Profile</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'ViewProfile',
+        data () {
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
